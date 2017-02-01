cc853dc0ec6ac1bc70049b51822301f0fb7f5d8e
Don Pollitz <chaos2007@gmail.com>
2017-01-31 19:45:46 -0600

    Have 2 examples for the stack trace.

--- 02_recursiveStackTraceExample/module02/module02.mk
+++ 02_recursiveStackTraceExample/module02/module02.mk
@@ -4,10 +4,10 @@ all: submodules
 submodules: submodule01 submodule02
 
 submodule01:
-	$(MAKE) -C submodule01
+	$(MAKE) -C submodule01 -f submodule01.mk
 
 submodule02:
-	$(MAKE) -C submodule02
+	$(MAKE) -C submodule02 -f submodule02.mk
 
 
 .PHONY: submodule01 submodule02 submodules all
